04b85a70364a221e507ef77dbb657f6dc349edb2
Kargin Nikita <hei@spark-mail.ru>
2016-10-07 09:39:32 +0300

    Homework 1 - done

--- src/Homework1.java
+++ src/Homework1.java
@@ -38,15 +38,7 @@ class Homework1 {
 
     static boolean isLeapYear(int year) {
 
-        if (year % 400 == 0) {
-            return true;
-        } else if (year % 100 == 0) {
-            return false;
-        } else if (year % 4 == 0) {
-            return true;
-        }
-
-        return false;
+        return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
 
     }
 
