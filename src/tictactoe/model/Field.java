767d445b6acb0c5d2ea63e1f73e0266a3dde4b6c
Kargin Nikita <hei@spark-mail.ru>
2016-10-12 02:51:10 +0300

    Created model for Field

---
+++ src/tictactoe/model/Field.java
@@ -0,0 +1,36 @@
+package tictactoe.model;
+
+import tictactoe.model.enums.Figure;
+
+public class Field {
+
+    private Figure[][] figures;
+    private final int SIZE;
+
+    public Field(int size) {
+        SIZE = size;
+        figures = new Figure[SIZE][SIZE];
+        initField();
+    }
+
+    public Figure[][] getFigures() {
+        return figures;
+    }
+
+    private void initField() {
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                figures[i][j] = Figure.EMPTY;
+            }
+        }
+    }
+
+    public void setFigure(Point point, Figure figure) {
+        figures[point.getX()][point.getY()] = figure;
+    }
+
+    public int getSIZE() {
+        return SIZE;
+    }
+
+}
