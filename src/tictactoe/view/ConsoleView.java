035a9dc4a151a86c3ea24039af67821eb0496182
Kargin Nikita <hei@spark-mail.ru>
2016-10-12 02:52:02 +0300

    Created the View. Can be another view, like GUI and oth.

---
+++ src/tictactoe/view/ConsoleView.java
@@ -0,0 +1,52 @@
+package tictactoe.view;
+
+import tictactoe.model.Point;
+import tictactoe.model.enums.Figure;
+import tictactoe.model.Game;
+
+import java.io.PrintStream;
+import java.util.Scanner;
+
+public class ConsoleView {
+
+    private Scanner scanner = new Scanner(System.in);
+
+    public PrintStream getStream() {
+        return stream;
+    }
+
+    private PrintStream stream = System.out;
+    private Game currentGame;
+
+    public ConsoleView(Game game) {
+        currentGame = game;
+    }
+
+    public void printField() {
+        for (Figure[] value : currentGame.getField().getFigures()) {
+            for (int i = 0; i < value.length; i++) {
+                stream.print((i != 2) ? value[i].getValue() + "|" : value[i].getValue());
+            }
+            stream.println();
+        }
+    }
+
+    public void printHello() {
+        stream.printf("Hello, %s!" + "\n", currentGame.getHumanPlayer().getName());
+        stream.println("Let's play!");
+    }
+
+    public Point getAMove() {
+
+        stream.println("Make a move! Coordinates x, y");
+        Point point = new Point(scanner.nextInt(), scanner.nextInt());
+        return point;
+
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        super.finalize();
+        scanner.close();
+    }
+}
