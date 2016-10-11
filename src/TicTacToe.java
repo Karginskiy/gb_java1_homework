8bbec9c7b98841877e925d93581f103a996e9c5b
Kargin Nikita <hei@spark-mail.ru>
2016-10-11 19:53:08 +0300

    Added constants.

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,8 +1,12 @@
 public class TicTacToe {
 
+    final char PLAYER_DOT = 'x';
+    final char AI = 'o';
+    final char EMPTY_DOT = '.';
+
     public static void main(String[] args) {
 
-
+        new TicTacToe();
 
     }
 
