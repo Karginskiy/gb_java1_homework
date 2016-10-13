3b14ff28d86783a06f68dbdfcc92accb627fbfdd
Kargin Nikita <hei@spark-mail.ru>
2016-10-13 23:18:51 +0300

    Changed game start.

--- src/tictactoe/Main.java
+++ src/tictactoe/Main.java
@@ -1,6 +1,8 @@
 package tictactoe;
 
 import tictactoe.controller.GameController;
+import tictactoe.model.Field;
+import tictactoe.model.Game;
 import tictactoe.model.enums.Difficulty;
 import tictactoe.model.enums.Figure;
 import tictactoe.model.players.AIPlayer;
@@ -10,11 +12,13 @@ public class Main {
 
     public static void main(String[] args) {
 
-        HumanPlayer player = new HumanPlayer("Nikita", Figure.X);
-        AIPlayer aiPlayer = new AIPlayer(Difficulty.HARD, Figure.O);
         int FIELD_SIZE = 3;
         int WIN_SERIES = 3;
-        GameController gameController = new GameController(player, aiPlayer, FIELD_SIZE, WIN_SERIES);
+
+        HumanPlayer player = new HumanPlayer("Nikita", Figure.X);
+        AIPlayer aiPlayer = new AIPlayer(Difficulty.HARD, Figure.O);
+        Game game = new Game(new Field(FIELD_SIZE), WIN_SERIES, aiPlayer, player);
+        GameController gameController = new GameController(game);
         gameController.start();
 
     }
