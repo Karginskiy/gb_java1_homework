1f50b15a87fbdc58a2713cea60f5ba36e91e3530
Kargin Nikita <hei@spark-mail.ru>
2016-10-14 00:15:33 +0300

    Creating the new AI move

---
+++ src/PrintFigure.java
@@ -0,0 +1,19 @@
+public class PrintFigure {
+    static Figure f;
+
+    public static void main(String[] args) {
+        System.out.print(f.KRESTIK.count + " " + f.Nolik.count + " ");
+    }
+}
+
+enum Figure {
+    KRESTIK, Nolik;
+    int count = 0;
+
+    Figure() {
+        System.out.print("x ");
+        count = count + 1;
+    }
+}
+
+// What result will shown?
