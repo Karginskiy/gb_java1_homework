c3590ff10de97b784b7e1ff41afc0698d38642a8
Kargin Nikita <hei@spark-mail.ru>
2016-10-14 00:21:10 +0300

    Move controller for AI

--- src/tictactoe/controller/AIMoveController.java
+++ src/tictactoe/controller/AIMoveController.java
@@ -19,7 +19,44 @@ class AIMoveController extends AbstractMoveController {
 
     void move() {
 
+        Random random = new Random();
 
+        switch (aiPlayer.getDifficulty()) {
+            case HARD:
+                for (int y = 0; y < Field.SIZE; y++) {
+                    for (int x = 0; x < Field.SIZE; x++) {
+                        if (isSet(new Point(x, y), anotherFigure(aiPlayer.getFigure()))) {
+                            for (int y_found = y; y_found < y + Field.SIZE; y_found++) {
+                                for (int x_found = x; x_found < x + Field.SIZE; x_found++) {
+                                    if (!((y == y_found) && (x == x_found))) {
+                                        if (isSet(new Point(x_found, y_found), anotherFigure(aiPlayer.getFigure()))) {
+                                            if (isSet(new Point(x * 2 - x_found, y * 2 - y_found), Figure.EMPTY)) {
+                                                field.setFigure(new Point((x * 2 - x_found), (y * 2 - y_found)), aiPlayer.getFigure());
+                                                return;
+                                            }
+                                            if (isSet(new Point(x_found * 2 - x, y_found * 2 - y), Figure.EMPTY)) {
+                                                field.setFigure(new Point((x_found * 2 - x), (y_found * 2 - y)), aiPlayer.getFigure());
+                                                return;
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            case EASY:
+                while (true) {
+                    int x_rand = random.nextInt(3);
+                    int y_rand = random.nextInt(3);
+                    Point point = new Point(x_rand, y_rand);
+                    if (isSet(point, Figure.EMPTY)) {
+                        this.field.setFigure(point, aiPlayer.getFigure());
+                        break;
+                    }
+                }
+                break;
+        }
 
     }
 
