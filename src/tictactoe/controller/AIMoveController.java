f72c1ec49b62d26117ec2b588162089f360d64d9
Kargin Nikita <hei@spark-mail.ru>
2016-10-14 15:28:28 +0300

    Added getToWinCoordinate method.

--- src/tictactoe/controller/AIMoveController.java
+++ src/tictactoe/controller/AIMoveController.java
@@ -19,7 +19,11 @@ class AIMoveController extends AbstractMoveController {
 
     void move() {
 
-
+        Point point = getToWinCoordinate(field);
+        if (point != null) {
+            field.setFigure(point, aiPlayer.getFigure());
+        }
+        point = getBlockingCoordinate(field);
 
     }
 
@@ -38,6 +42,110 @@ class AIMoveController extends AbstractMoveController {
         }
 
         return null;
+    }
+
+    private Point getBlockingCoordinate(final Field field) {
+
+
+        return null;
+
+    }
+
+    private Point getToWinCoordinate(final Field field) {
+
+        // BEGIN (write your solution here)
+        for (int i = 0; i < Field.SIZE; i++) {
+            int index = checkHorizontalsForWinSituations(field, i);
+            if (index != -1) {
+                for (int j = 0; j < Field.SIZE; j++) {
+                    if (isSet(new Point(index, j), Figure.EMPTY)) {
+                        return new Point(index, j);
+                    }
+                }
+            }
+            index = checkVerticalsForWinSituations(field, i);
+            if (index != -1) {
+                for (int j = 0; j < Field.SIZE; j++) {
+                    if (isSet(new Point(j, index), Figure.EMPTY)) {
+                        return new Point(j, index);
+                    }
+                }
+            }
+            index = checkDiagonalsForWinSituations(field, i);
+            if (index == 1) {
+                for (int j = 0; j < Field.SIZE; j++) {
+                    if (isSet(new Point(j, j), Figure.EMPTY)) {
+                        return new Point(j, j);
+                    }
+                }
+            } else if (index == 2) {
+                for (int j = 0, k = Field.SIZE - 1; j < Field.SIZE; j++, k--) {
+                    if (isSet(new Point(j, k), Figure.EMPTY)) {
+                        return new Point(j, k);
+                    }
+                }
+            }
+
+        }
+
+        return null;
+        // END
+    }
+
+    private int checkHorizontalsForWinSituations(Field field, int indexOfLine) {
+
+        int counter = 0;
+        for (int j = 0; j < Field.SIZE; j++) {
+
+            if (isSet(new Point(indexOfLine, j), aiPlayer.getFigure())) {
+                counter++;
+            }
+            if (counter > 1) {
+                return indexOfLine;
+            }
+        }
+
+        return -1;
+
+    }
+
+    private int checkVerticalsForWinSituations(Field field, int indexOfLine) {
+
+        int counter = 0;
+        for (int j = 0; j < Field.SIZE; j++) {
+            if (isSet(new Point(j, indexOfLine), aiPlayer.getFigure())) {
+                counter++;
+            }
+        }
+
+        if (counter > 1) {
+            return indexOfLine;
+        }
+
+        return -1;
+
+    }
+
+    private int checkDiagonalsForWinSituations(Field field, int indexOfDiagonal) {
+
+        int counter = 0;
+        if (indexOfDiagonal == 1) {
+            for (int i = 0; i < Field.SIZE; i++) {
+                if (isSet(new Point(i, i), aiPlayer.getFigure())) {
+                    counter++;
+                }
+            }
+        } else if (indexOfDiagonal == 2) {
+            for (int i = 0, k = Field.SIZE - 1; i < Field.SIZE; i++, k--) {
+                if (isSet(new Point(i, k), Figure.X)) {
+                    counter++;
+                }
+            }
+        }
+
+        if (counter > 1) return indexOfDiagonal;
+
+        return -1;
     }
 
 
