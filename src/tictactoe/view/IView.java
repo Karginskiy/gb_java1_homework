d560ebdd363146e716a1e9f8baa0765a0d62f354
Kargin Nikita <hei@spark-mail.ru>
2016-10-15 12:55:34 +0300

    IView is make easier dependency.

--- src/tictactoe/view/IView.java
+++ src/tictactoe/view/IView.java
@@ -3,10 +3,11 @@ package tictactoe.view;
 import tictactoe.model.Point;
 import tictactoe.model.players.AbstractPlayer;
 
-interface IView {
+public interface IView {
 
     Point getAMove();
     void printWinner(AbstractPlayer player);
-
+    void printField();
+    void printTurnPlayerName(AbstractPlayer player);
 
 }
