d0d2c5f74fed1eefab9678bb22abde6dd8f6851d
Kargin Nikita <hei@spark-mail.ru>
2016-10-15 16:39:01 +0300

    Added SimpleTicTacToe

---
+++ src/SimpleTicTacToe.java
@@ -0,0 +1,18 @@
+import tictactoe.model.Field;
+
+import java.util.Scanner;
+
+/**
+ *
+ * simple TicTacToe
+ *
+ * @author Kargin Nikita
+ * @version 0.1
+ *
+ */
+
+public class SimpleTicTacToe {
+
+
+
+}
