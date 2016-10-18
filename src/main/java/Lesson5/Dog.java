08e9dc261e749d629b8cf490297723179935b101
Kargin Nikita <hei@spark-mail.ru>
2016-10-18 22:21:22 +0300

    Changed Dog

--- src/main/java/Lesson5/Dog.java
+++ src/main/java/Lesson5/Dog.java
@@ -1,6 +1,18 @@
 package Lesson5;
 
-public class Dog {
+class Dog extends Animal {
+
+    private Speed speed;
+
+    Dog(String name, boolean canSwim, double length, Speed speed) {
+        super(name, canSwim, length);
+        this.speed = speed;
+    }
+
+    @Override
+    protected void run() {
+        System.out.println("Dog runs " + speed);
+    }
 
 
 
