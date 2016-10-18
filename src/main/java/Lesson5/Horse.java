f5f5b888ead1d05c98ab70fc2ae7b413bb6aa505
Kargin Nikita <hei@spark-mail.ru>
2016-10-18 21:57:43 +0300

    Added Horse

---
+++ src/main/java/Lesson5/Horse.java
@@ -0,0 +1,26 @@
+package Lesson5;
+
+public class Horse extends AbstractAnimal {
+
+    Horse(String name, Speed speed, boolean canSwim, double length) {
+        super(name, speed, canSwim, length);
+    }
+
+    @Override
+    protected void run() {
+
+    }
+
+    @Override
+    protected void swim() {
+
+    }
+
+    @Override
+    protected void leap() {
+
+    }
+
+
+
+}
