e082fe7bb670d92ae2b6e93b3436087cfea881cb
Kargin Nikita <hei@spark-mail.ru>
2016-10-18 22:21:01 +0300

    Changed animal

--- src/main/java/Lesson5/Animal.java
+++ src/main/java/Lesson5/Animal.java
@@ -1,12 +1,19 @@
 package Lesson5;
 
-public class Animal {
+abstract class Animal {
 
     private String name;
+    private boolean canSwim;
+    private double length;
 
-    public Animal(String name) {
+    Animal(String name, boolean canSwim, double length) {
         this.name = name;
+        this.canSwim = canSwim;
+        this.length = length;
     }
+
+    // Overriding representation
+    abstract protected void run();
 
     public String getName() {
         return name;
@@ -15,4 +22,31 @@ public class Animal {
     public void setName(String name) {
         this.name = name;
     }
+
+    // Representation of inheritance
+    void swim() {
+        if (this.isCanSwim()) {
+            System.out.println(this.name + " swimming!");
+        } else {
+            System.out.println(this.name + " can't swim. Teach him! :)");
+        }
+    }
+
+    // Representation of inheritance
+    void leap() {
+        System.out.printf(this.name + " jumps on %.2f\n", this.getLength() * 1.66);
+    }
+
+    private boolean isCanSwim() {
+        return canSwim;
+    }
+
+    private double getLength() {
+        return length;
+    }
+
+    enum Speed {
+        fast, middling, slow;
+    }
+
 }
