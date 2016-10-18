17406ada6e1a98072692e9c721b123a53388ce04
Kargin Nikita <hei@spark-mail.ru>
2016-10-18 22:21:13 +0300

    Changed Cat

--- src/main/java/Lesson5/Cat.java
+++ src/main/java/Lesson5/Cat.java
@@ -2,15 +2,16 @@ package Lesson5;
 
 public class Cat extends Animal {
 
-    public Cat(String name) {
-        super(name);
+    private Speed speed;
+
+    Cat(String name, boolean canSwim, double length, Speed speed) {
+        super(name, canSwim, length);
+        this.speed = speed;
     }
 
-    public static void main(String[] args) {
-
-
-
+    @Override
+    protected void run() {
+        System.out.println("Cat runs " + speed);
     }
-
 
 }
