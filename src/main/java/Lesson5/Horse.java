07fc355b0246de26f06bf6dfdbea1f703e041769
Kargin Nikita <hei@spark-mail.ru>
2016-10-19 17:26:24 +0300

    Changed Horse output

--- src/main/java/Lesson5/Horse.java
+++ src/main/java/Lesson5/Horse.java
@@ -4,9 +4,10 @@ class Horse extends Animal {
 
     private Speed speed;
 
-    Horse(String name, boolean canSwim, double length, Speed speed) {
-        super(name, canSwim, length);
+    Horse(String name, boolean canSwim, double height, Speed speed) {
+        super(name, canSwim, height);
         this.speed = speed;
+        this.jumphigh = height * 1.1;
     }
 
     @Override
