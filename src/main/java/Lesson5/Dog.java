c273c5562f1140c111c1b68a4f309b07664574e9
Kargin Nikita <hei@spark-mail.ru>
2016-10-19 17:26:21 +0300

    Changed jumpheight

--- src/main/java/Lesson5/Dog.java
+++ src/main/java/Lesson5/Dog.java
@@ -4,9 +4,10 @@ class Dog extends Animal {
 
     private Speed speed;
 
-    Dog(String name, boolean canSwim, double length, Speed speed) {
-        super(name, canSwim, length);
+    Dog(String name, boolean canSwim, double height, Speed speed) {
+        super(name, canSwim, height);
         this.speed = speed;
+        this.jumphigh = height * 1.2;
     }
 
     @Override
