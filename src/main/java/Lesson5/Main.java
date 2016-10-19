15104d5576d26bc569ffd95b0f17d33138eea005
Kargin Nikita <hei@spark-mail.ru>
2016-10-19 17:26:10 +0300

    Changed Main input

--- src/main/java/Lesson5/Main.java
+++ src/main/java/Lesson5/Main.java
@@ -5,14 +5,16 @@ public class Main {
     public static void main(String[] args) {
 
         Animal[] animals = new Animal[3];
-        animals[0] = new Cat("Shreddinger", false, 70, Animal.Speed.middling);
-        animals[1] = new Dog("Dirt", true, 120, Animal.Speed.slow);
-        animals[2] = new Horse("Speedy", true, 170, Animal.Speed.fast);
+        animals[0] = new Cat("Shreddinger", false, 0.3, Animal.Speed.middling);
+        animals[1] = new Dog("Dirt", true, 0.5, Animal.Speed.slow);
+        animals[2] = new Horse("Speedy", true, 1.2, Animal.Speed.fast);
 
         for (Animal animal : animals) {
             animal.run();
             animal.swim();
-            animal.leap();
+            animal.leap(0.5);
+            animal.leap(1);
+            animal.leap(1.5);
         }
 
     }
