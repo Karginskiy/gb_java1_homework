aecbde0485c722e28c28fbf44dc1c2f2193c7495
Kargin Nikita <hei@spark-mail.ru>
2016-10-22 14:44:10 +0300

    String constructors

---
+++ src/main/java/Lesson6/StringAndFile.java
@@ -0,0 +1,16 @@
+package Lesson6;
+
+public class StringAndFile {
+
+    public static void main(String[] args) {
+
+        char[] c = {'J', 'a', 'v', 'a'};
+        String s1 = new String(c);
+        String s2 = new String(s1);
+        String s = "";
+        System.out.println(s1);
+        System.out.println(s2);
+
+    }
+
+}
