36a14f8cb8fa5ac223729712c542b9b7540f85b4
Kargin Nikita <hei@spark-mail.ru>
2016-10-23 22:39:49 +0300

    Added merger for 2 files

---
+++ src/main/java/Lesson6/Merger.java
@@ -0,0 +1,49 @@
+package Lesson6;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class Merger {
+
+    private final String fileNameOne;
+    private final String fileNameTwo;
+
+    Merger(final String fileNameOne, final String fileNameTwo) {
+        this.fileNameOne = fileNameOne;
+        this.fileNameTwo = fileNameTwo;
+    }
+
+    public void merge() {
+
+        try {
+            writeInFile(fileNameOne, getFullFile(fileNameTwo));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+    private void writeInFile(String fileName, byte[] buffer) throws IOException {
+
+        FileOutputStream fileOutputStream = new FileOutputStream(fileName, true);
+        fileOutputStream.write(buffer);
+        fileOutputStream.close();
+
+    }
+
+    private byte[] getFullFile(String fileName) throws IOException {
+
+        byte[] buffer = null;
+
+        FileInputStream inputStream = new FileInputStream(fileName);
+        buffer = new byte[inputStream.available()];
+        inputStream.read(buffer);
+        inputStream.close();
+
+        return buffer;
+
+    }
+
+}
