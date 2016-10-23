86a6d5e21db03bb86f3b53a2dae87bc00d87a66a
Kargin Nikita <hei@spark-mail.ru>
2016-10-23 22:10:22 +0300

    Changes to the Main. Now with fileName to serialize the current state of the game

--- src/main/java/tictactoe/Main.java
+++ src/main/java/tictactoe/Main.java
@@ -14,10 +14,11 @@ public class Main {
 
         int FIELD_SIZE = 3;
         int WIN_SERIES = 3;
+        String fileName = "F:\\WebProjects\\gb_java1_homework\\src\\main\\java\\tictactoe\\memory\\field";
 
         HumanPlayer player = new HumanPlayer("Nikita", Figure.X);
         AIPlayer aiPlayer = new AIPlayer(Difficulty.HARD, Figure.O);
-        Game game = new Game(new Field(FIELD_SIZE), WIN_SERIES, aiPlayer, player);
+        Game game = new Game(new Field(FIELD_SIZE), WIN_SERIES, aiPlayer, player, fileName);
         GameController gameController = new GameController(game);
         gameController.start();
 
